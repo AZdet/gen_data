d8b74c4a334953b36d75e0978b26a9a179970d72
shenghao <shzhou2@seas.upenn.edu>
2020-02-24 21:54:09 -0500

    Add feature and Fix bug

--- generate_data.py
+++ generate_data.py
@@ -4,7 +4,7 @@ from skimage import transform as ski_tf
 import matplotlib.pyplot as plt
 from numpy.random import randint, rand
 import os
-
+from matplotlib.patches import Rectangle
 eps = 3e-2 # threshold for mask to be taken as 1
 
 
@@ -32,24 +32,53 @@ def getPathFromTxt(txt):
             yield path_lst[idx][:-1]
 
 
-
 def getBBoxFromMask(mask, eps=eps):
     Y, X = np.mgrid[0:mask.shape[0], 0:mask.shape[1]]
     mask_X = X[mask > eps]
     mask_Y = Y[mask > eps]
+    if mask_X.size == 0 or mask_Y.size == 0:
+        return None
     return np.array([[mask_X.min(), mask_Y.min()], [mask_X.max(), mask_Y.max()]])
 
-def unionBBox(bbox1, bbox2, low=0.1, high=0.9):
-    in_lt = np.maximum(bbox1[0, :], bbox2[0, :])
-    in_rb = np.minimum(bbox1[1, :], bbox2[1, :])
-    un_lt = np.minimum(bbox1[0, :], bbox2[0, :])
-    un_rb = np.maximum(bbox1[1, :], bbox2[1, :])
+def unionBBox(bbox1, bbox2, low=0.2, high=0.9):
+    # bbox[0] is x_min, y_min; bbox[1] is x_max, y_max
+    in_lt = np.maximum(bbox1[0], bbox2[0])
+    in_rb = np.minimum(bbox1[1], bbox2[1])
+    un_lt = np.minimum(bbox1[0], bbox2[0])
+    un_rb = np.maximum(bbox1[1], bbox2[1])
     wh = (in_rb - in_lt).clip(0)
     inter_area = np.multiply.reduce(wh)
-    union_area = np.multiply.reduce(un_rb - un_lt) - inter_area
+    union_area = np.multiply.reduce(bbox1[1]-bbox1[0]) + np.multiply.reduce(bbox2[1] - bbox2[0]) - inter_area
     iou = inter_area / union_area
     if low <= iou <= high:
-        return np.c_[un_lt, un_rb].T
+        # if in_lt[1] == bbox1[0, 1] and in_rb[1] == bbox1.all():
+        #     import pdb; pdb.set_trace()
+        #     return np.c_[bbox1[0], np.r_[in_rb[0], in_lt[1]]] 
+        # import pdb; pdb.set_trace()
+        # Y, X = np.mgrid[bbox1[0, 1]: bbox1[1, 1]+1, bbox1[0, 0]:bbox1[1, 0]+1]
+        # inside_inter = np.logical_and(
+        #     np.logical_and(X >= in_lt[0], X <= in_rb[0]), 
+        #     np.logical_and(Y >= in_lt[1], Y <= in_rb[1])
+        #     )
+        # #import pdb; pdb.set_trace()
+        # X_good = X[np.logical_not(inside_inter)]
+        # Y_good = Y[np.logical_not(inside_inter)]
+        # if X_good.size == 0 or Y_good.size == 0:
+        #     return None
+        # # hack fix, add in the bottom part 
+        # # if bbox1[1,1] - Y_good.max() > 20:
+        # #     return np.array([[X_good.min(), Y_good.min()], [X_good.max(), Y_good.max()+10]])
+        # res = np.array([[X_good.min(), Y_good.min()], [X_good.max(), Y_good.max()]])
+        # ax = plt.gca()
+        # rect1 = Rectangle((bbox1[0, 0], bbox1[0,1]), bbox1[1,0]-bbox1[0,0], bbox1[1,1]-bbox1[0,1], edgecolor='r',fill=False)
+        # rect2 = Rectangle((bbox2[0, 0], bbox2[0,1]), bbox2[1,0]-bbox2[0,0], bbox2[1,1]-bbox2[0,1], edgecolor='r', fill=False)        
+        # rect3 = Rectangle((res[0, 0], res[0,1]), res[1,0]-res[0,0], res[1,1]-res[0,1], edgecolor='r',fill=False)
+        # ax.add_patch(rect1)
+        # ax.add_patch(rect2)
+        # ax.add_patch(rect3)
+        res = bbox1
+        return res
+        
     else:
         # bbox1 and bbox2 have no overlap
         return None
@@ -60,22 +89,22 @@ def transform(mask, mode="p2c"):
     where p stands for person, c stands for car and r stands for random object
     '''
     if mode == "p2c":
-        tx, ty = randint(-50, 50), 0
+        tx, ty = randint(-10, 20), 0
         rot = randint(-10, 10) * np.pi / 180  # from degree to radian
         scale = rand() * 0.4 + 0.6  # (0.6, 1)
     elif mode == "c2c":
-        tx, ty = randint(-50, 50), 0
+        tx, ty = randint(-20, 30), randint(20, 40)
         rot = 0
-        scale = 1
+        scale = 1.2
     elif mode == "p2p":
-        tx, ty = randint(-50, 50), 0
+        tx, ty = randint(-20, 30), randint(10, 20)
         rot = 0
         scale = rand() * 0.4 + 0.6  # (0.6, 1)
     elif mode == "c2p":
-        tx, ty = 0, randint(-10, 50)
+        tx, ty = randint(-30, 30), randint(20, 40)
         rot = 0
-        scale = 1
-    elif mode == "r2c" or mode == "r2p":
+        scale = 1.2
+    elif mode == "random":
         tx, ty = randint(-50, 50), randint(-50, 50)
         rot = randint(-10, 10) * np.pi / 180
         scale = rand() * 0.4 + 0.6 
@@ -92,52 +121,185 @@ def genOccluded(mask1, img1, mask2, mode="p2c", out_size=128):
     mask2 is the object in the front and creates occlusion.
     '''
     out = np.zeros((out_size, out_size, 3))
-    while True:
-        mask2_transformed = transform(mask2, mode)
-        fig, ax = plt.subplots(1, 2)
-        # ax[0].imshow(mask2)
-        # ax[1].imshow(mask2_transformed)
+    
+    mask2_transformed = transform(mask2, mode)
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(mask2)
+    ax[1].imshow(mask2_transformed)
+    plt.show()
+    #import pdb; pdb.set_trace()
+    bbox1 = getBBoxFromMask(mask1)
+    bbox2 = getBBoxFromMask(mask2_transformed)
+    if bbox1 is None or bbox2 is None:
+        return None
+    union_area = unionBBox(bbox1, bbox2)
+    if union_area is not None:
+        res = img1
+        res[mask2_transformed > eps] = 0  # occlusion 
+        plt.imshow(res)
+        plt.show()
+        res = res[union_area[0,1] : union_area[1,1], union_area[0,0]:union_area[1,0]]
+        # plt.imshow(union_area)
         # plt.show()
-        bbox1 = getBBoxFromMask(mask1)
-        bbox2 = getBBoxFromMask(mask2_transformed)
-        union_area = unionBBox(bbox1, bbox2)
-        if union_area is not None:
-            res = img1
-            res[mask2_transformed > eps] = 0  # occlusion effect
-            res = res[union_area[0,1] : union_area[1,1], union_area[0,0]:union_area[1,0]]
-            plt.imshow(res)
-            plt.show()
-            w = union_area[1, 0] - union_area[0, 0]
-            h = union_area[1, 1] - union_area[0, 1]
-            if w > h:
-                scale = out_size / w
-                res_scale = ski_tf.rescale(res, scale)
-                out[out_size//2 - res_scale.shape[0]//2: out_size//2 + (res_scale.shape[0]-res_scale.shape[0]//2), :] = res_scale
-            else:
-                scale = out_size / h
-                res_scale = ski_tf.rescale(res, scale)
-                out[:, out_size//2 - res_scale.shape[1]//2: out_size//2 +(res_scale.shape[1]-res_scale.shape[1]//2)] = res_scale
-            break
-    return out
+        Y, X = np.mgrid[0:res.shape[0], 0:res.shape[1]]
+        # import pdb; pdb.set_trace()
+        res_nonzero = (res > eps).any(axis=2)
+        X_good = X[res_nonzero]
+        Y_good = Y[res_nonzero]
+        if (res_nonzero).sum() == 0 or X_good.size == 0 or Y_good.size == 0:
+            return None
+        res = res[Y_good.min():Y_good.max(), X_good.min():X_good.max()]
+        plt.imshow(res)
+        plt.show()
+        w = X_good.max() - X_good.min() #union_area[1, 0] - union_area[0, 0]
+        h = Y_good.max() - Y_good.min() #union_area[1, 1] - union_area[0, 1]
+        if w == 0 or h == 0:
+            return None
+        print(max(w/h, h/w))
+        #import pdb; pdb.set_trace()
+
+        # if max(w/h, h/w) > 2.1:
+        #     return None
+        if w > h:
+            scale = out_size / w
+            res_scale = ski_tf.rescale(res, scale, order=3, multichannel=True)
+            out[out_size//2 - res_scale.shape[0]//2: out_size//2 + (res_scale.shape[0]-res_scale.shape[0]//2), :] = res_scale
+        else:
+            scale = out_size / h
+            res_scale = ski_tf.rescale(res, scale, order=3, multichannel=True)
+            out[:, out_size//2 - res_scale.shape[1]//2: out_size//2 +(res_scale.shape[1]-res_scale.shape[1]//2)] = res_scale
+        #import pdb; pdb.set_trace()
+        return out
+    else:
+        return None   
+    
 
 
-def generateData():
-    car_gen = getPathFromTxt('real_car.txt')
-    person_gen = getPathFromTxt('real_person.txt')
+def generateData(mode, n_iter, save_dir):
+    txt2load1 = {'c2c': 'real_car.txt', 
+                'c2p': 'real_person.txt',
+                'p2c':'real_car.txt',
+                'p2p': 'real_person.txt'}
+    txt2load2 = {'c2c': 'real_car.txt', 
+                'c2p': 'real_car.txt',
+                'p2c':'real_person.txt',
+                'p2p': 'real_person.txt'}
+    gen1 = getPathFromTxt(txt2load1[mode])
+    gen2 = getPathFromTxt(txt2load2[mode])
     img_folder = 'full'
     mask_folder = 'full_mask'
-    out_dir = "out"
-    out_mask_dir = "out_mask"
-    for car_path, person_path in zip(car_gen, person_gen):
-        car_img = plt.imread(os.path.join(img_folder, car_path))
-        car_mask = plt.imread(os.path.join(mask_folder, car_path))[:,:,0]
-        person_mask = plt.imread(os.path.join(img_folder, person_path))[:,:,0]
-        out = genOccluded(car_mask, car_img, person_mask)
+    out_dir = mode
+    out_mask_dir = "mask_" + mode
+    N_ITER = n_iter
+
+    i = 0
+    for path1, path2 in zip(gen1, gen2):
+        if i == N_ITER:
+            break
+        print(path1)
+        print(path2)
+        while True:
+            car_img = plt.imread(os.path.join(img_folder, path1))
+            car_mask = plt.imread(os.path.join(mask_folder, path1))[:,:,0]
+            person_mask = plt.imread(os.path.join(img_folder, path2))[:,:,0]
+            out = genOccluded(car_mask, car_img, person_mask, mode)
+            if out is None:
+                path1 = next(gen1)
+                path2 = next(gen2)
+            else:
+                break
+        plt.imshow(out)
+        plt.show()
         out_mask = out > eps
-        plt.imsave(os.path.join(out_dir, car_path), out)
-        plt.imsave(os.path.join(out_mask_dir, out_mask), out_mask)
+        plt.imsave(os.path.join(save_dir, out_dir, str(i)+path1), out)
+        plt.imsave(os.path.join(save_dir, out_mask_dir, str(i)+path1), out_mask.astype(np.float32))
+        print('========')
+        i += 1
         
+def generateDataRandomMask(n_iter, save_dir):
+    mode = 'random'
+    txt2load = ['real_car.txt', 'real_person.txt']
+    if rand() < 0.5:
+        txt = txt2load[0]
+    else:
+        txt = txt2load[1]
+    gen1 = getPathFromTxt(txt)
+    img_folder = 'full'
+    mask_folder = 'full_mask'
+    out_dir = mode
+    out_mask_dir = "mask_" + mode
+    N_ITER = n_iter
+
+    i = 0
+    for path1 in gen1:
+        if i == N_ITER:
+            break
+        print(path1)
+        while True:
+            target_img = plt.imread(os.path.join(img_folder, path1))
+            target_mask = plt.imread(os.path.join(mask_folder, path1))[:,:,0]
+            mask = get_random_mask(target_mask)
+            out = genOccluded(target_mask, target_img, mask, mode)
+            if out is None:
+                path1 = next(gen1)
+            else:
+                break
+        plt.imshow(out)
+        plt.show()
+        out_mask = out > eps
+        plt.imsave(os.path.join(save_dir, out_dir, str(i)+path1), out)
+        plt.imsave(os.path.join(save_dir, out_mask_dir, str(i)+path1), out_mask.astype(np.float32))
+        print('========')
+        i += 1
+
+def vis_img_arr(imgs, name):
+    length = len(imgs)
+    fig, axes = plt.subplots(1, length)
+    for i, ax in enumerate(axes):
+        ax.imshow(imgs[i])
+        ax.axis('off')
+    fig.savefig(name, dpi=fig.dpi)
+    plt.show()
+
+def get_random_mask(mask1):
+    '''
+    generate random masks to occlude target objects
+    '''
+    if rand() < 0.5:
+        mask = np.zeros_like(mask1, dtype=bool)
+        width = randint(5, 15)
+        height = randint(40, 127)
+        x = randint(10,110)
+        mask[-height:-1, x:x+width] = 1
+    else:
+        masks = ['random.png', 'random2.png', 'random3.png','random4.png', 'random5.png']
+        length = len(masks)
+        idx = randint(length)
+        mask = plt.imread(masks[idx])
+    import pdb; pdb.set_trace()
+    return mask
+
+
+
 
 
 if __name__ == "__main__":
-    generateData()
+    for mode in ['p2c']:
+        generateData(mode, 100000, 'train')
+        generateData(mode, 10000, 'test')
+    for mode in ['c2p']:
+        generateData(mode, 50000, 'train')
+        generateData(mode, 5000, 'test')
+
+    for mode in ['c2c', 'p2p']:
+        generateData(mode, 80000, 'train')
+        generateData(mode, 10000, 'test')
+        # gen = getDataPath('out_'+mode)
+        # length = 5
+        # imgs = []
+        # for _ in range(length):
+        #     img_path = next(gen)
+        #     img = plt.imread(str(img_path))
+        #     imgs.append(img)
+        # vis_img_arr(imgs, name=mode+'.png')
+    
